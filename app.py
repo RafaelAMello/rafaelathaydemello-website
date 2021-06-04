2635190d7e97214d718b7117c868de41abb70edd
RafaelAMello <rafaelathaydemello@gmail.com>
2021-06-04 14:49:02 +1000

    Configure Streamlit a bit more

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+# from src import WELCOME_PAGE
 
-st.write("""
-# Welcome to Rafael Mello's Website
-""")
+# Hide footer and menu, No need in my website
+st.markdown("""<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;} </style>""", unsafe_allow_html=True)
+'''
+# Hello, welcome to my website
+My name is Rafael Mello, I work as a Data Engineer in Melbourne, Australia
+
+I made this website mostly mostly for fun and a little for profit
+'''
+st.sidebar.title("Contents")
