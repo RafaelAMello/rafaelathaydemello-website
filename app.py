5dad4ee3bf4392366aa28de10e06e707f7e206da
RafaelAMello <rafaelathaydemello@gmail.com>
2021-06-04 10:47:34 +1000

    Rename main to app

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ st.write("""
 # Welcome to Rafael Mello's Website
 """)
 
-
 # from flask import Flask
 
 # app = Flask(__name__)
 
-# app.route('/hello')
+# app.route('')
 # def hello():
 #     return "hello"
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8051)
+# # if __name__ == '__main__':
+# #     app.run(host='localhost', port=8051)
