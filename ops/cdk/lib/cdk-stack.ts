61052b8a80da7a054fb40b0de19fd1180d396028
RafaelAMello <rafaelathaydemello@gmail.com>
2021-06-06 11:43:02 +1000

    Configure cdk

--- ops/cdk/lib/cdk-stack.ts
+++ ops/cdk/lib/cdk-stack.ts
@@ -1,9 +1,29 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as ecs from '@aws-cdk/aws-ecs'
+import * as ecr from '@aws-cdk/aws-ecr'
+import * as route53 from '@aws-cdk/aws-route53'
+import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns'
 
-export class CdkStack extends cdk.Stack {
+export class StreamlitApp extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const ecrRepo = ecr.Repository.fromRepositoryName(this, 'StreamlitRepo', 'personal-website/streamlit-app')
+    const hostedZone = route53.HostedZone.fromHostedZoneId(this, 'HostedZone', 'Z03972782S02DJS3QFINK')
+
+    new ApplicationLoadBalancedFargateService(
+      this,
+      'streamlitApp',
+      {
+        desiredCount: 1,
+        domainZone: hostedZone,
+        listenerPort: 8051,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromEcrRepository(ecrRepo),
+          containerName: 'StreamlitApp',
+          containerPort: 8051
+        }
+      }
+      )
   }
 }
