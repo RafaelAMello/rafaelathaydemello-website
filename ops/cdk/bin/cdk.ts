0259ecf0770a5a88b2891975b3c7fe220e538ea2
RafaelAMello <rafaelathaydemello@gmail.com>
2021-06-06 18:05:55 +1000

    No env

--- ops/cdk/bin/cdk.ts
+++ ops/cdk/bin/cdk.ts
@@ -3,12 +3,20 @@ import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
 import { StreamlitApp, StreamlitEcr } from '../lib/cdk-stack'
 
+const getEnvVariable = (envVarName: string): string => {
+    const envVar = process.env[envVarName]
+    if (envVar === undefined) {
+        throw new Error(`${envVarName} Variables not set`)
+    }
+    return envVar
+}
+
 const app = new cdk.App()
 new StreamlitEcr(app, 'StreamlitEcr')
 new StreamlitApp(app, 'StreamlitApp', {
     env: {
-        account: process.env['CDK_DEFAULT_ACCOUNT'],
-        region: process.env['CDK_DEFAULT_REGION'],
+        account: getEnvVariable('CDK_DEFAULT_ACCOUNT'),
+        region: getEnvVariable('CDK_DEFAULT_REGION'),
     },
-    imageTag: process.env['GITHUB_RUN_ID'] || 'latest',
+    imageTag: getEnvVariable('GITHUB_RUN_ID')
 })
