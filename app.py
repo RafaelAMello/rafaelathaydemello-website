417ad0df60822fd2bf55d0861152b2b79bea069e
RafaelAMello <rafaelathaydemello@gmail.com>
2021-06-15 16:47:14 +1000

    Send actual messages to slack

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from collections import OrderedDict
 
 import streamlit as st
@@ -10,6 +11,7 @@ from src.about_me import about_me
 from src.tech_stack import tech_stack
 from src.data_science import data_science_adventures
 
+SLACK_URL = os.environ['SLACK_URL']
 st.set_page_config(page_title="Raf's Website", page_icon="📊")
 # Hide footer and menu, No need in my website
 st.markdown("""<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;} </style>""", unsafe_allow_html=True)
@@ -26,7 +28,7 @@ CHOICE = st.sidebar.radio("Go to", options=list(pages.keys()))
 pages[CHOICE]()
 
 form = st.sidebar.form(key='contact_form')
-form.markdown("## Get in touch (this does nothing)")
+form.markdown("## Get in touch")
 email = form.text_input(label='whats your email?')
 text = form.text_area(label='Tell me whats on your mind')
 contact_submitted = form.form_submit_button('Send off')
@@ -35,5 +37,6 @@ if contact_submitted:
     try:
         valid = validate_email(email)
         email = valid.email
+        requests.post(SLACK_URL, json={'text' : f"{email} wants to chat, they said: \n {text}"})
     except EmailNotValidError as e:
         st.sidebar.warning(e)
