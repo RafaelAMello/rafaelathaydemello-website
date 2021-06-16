64facde92bc76b343b7a598ad5f06f4b65c5113f
RafaelAMello <rafaelathaydemello@gmail.com>
2021-06-16 20:33:59 +1000

    Update tech stack info

--- src/tech_stack.py
+++ src/tech_stack.py
@@ -3,8 +3,11 @@ import streamlit as st
 def tech_stack():
     st.markdown("""
     # The tech stack
-    ## On selling your soul to jeff bazos in exchange for cloud infrastructure
-    TODO
+    The main purpose of this setup is to explore how I would setup a streamlit app with AWS archetecture.
+
+    I don't particularly enjoy selling my soul to jeff bazos in exchange for cloud infrastructure but it seems particularly useful these days.
+
+    If anyone knows a more ethical cloud provider then let me know!
     ## The AWS Archetecture diagram
     TODO
     ## Live billing stats
